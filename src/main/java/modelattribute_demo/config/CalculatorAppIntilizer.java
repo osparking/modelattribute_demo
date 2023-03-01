7d08566902a10cb08160e5bdeb3f1a609a13140e
osparking <jbpark03@gmail.com>
2023-03-01 14:39:05 +0900

    calculator initializer default code

---
+++ src/main/java/modelattribute_demo/config/CalculatorAppIntilizer.java
@@ -0,0 +1,25 @@
+package modelattribute_demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class CalculatorAppIntilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
