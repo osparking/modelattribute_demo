9de1abcd30ec5b44578171bb6afbaafc8393bccb
osparking <jbpark03@gmail.com>
2023-03-01 15:47:37 +0900

    extra annotation added

--- src/main/java/modelattribute_demo/config/CalculatorAppConfig.java
+++ src/main/java/modelattribute_demo/config/CalculatorAppConfig.java
@@ -3,8 +3,11 @@ package modelattribute_demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "modelattribute_demo.controllers")
 public class CalculatorAppConfig {
