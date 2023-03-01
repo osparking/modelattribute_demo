6c6214894126c13a226de4423c27d1c32ece0125
osparking <jbpark03@gmail.com>
2023-03-01 14:52:22 +0900

    application configuration

---
+++ src/main/java/modelattribute_demo/config/CalculatorAppConfig.java
@@ -0,0 +1,10 @@
+package modelattribute_demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "modelattribute_demo.controllers")
+public class CalculatorAppConfig {
+
+}
