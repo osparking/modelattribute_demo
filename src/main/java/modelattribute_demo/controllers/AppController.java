9cf1d3116d136665c99d4f7515ecfd654ddbcce7
osparking <jbpark03@gmail.com>
2023-03-01 21:40:54 +0900

    application controller class

---
+++ src/main/java/modelattribute_demo/controllers/AppController.java
@@ -0,0 +1,27 @@
+package modelattribute_demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import modelattribute_demo.dto.NameInfoDTO;
+
+@Controller
+public class AppController {
+	@RequestMapping("/home")
+	public String showHomePage(Model model) {
+		// DTO 에서 존재하는 성질 값들을 읽어오라.
+		NameInfoDTO nameInfoDTO = new NameInfoDTO();
+		model.addAttribute("nameInfo", nameInfoDTO);
+
+		return "welcome-page";
+	}
+
+	@RequestMapping("/home")
+	public String showResultPage(NameInfoDTO nameInfoDTO, Model model) {
+		// URL 에서 읽은 값들을 모델에 성질 값으로 저장하라
+		model.addAttribute("nameInfo", nameInfoDTO);
+
+		return "result-page";
+	}
+}
