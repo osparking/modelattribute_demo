26e03cef0dfc46db8f2501409d64ae4804322183
osparking <jbpark03@gmail.com>
2023-03-02 21:31:41 +0900

    method change using modelattribute

--- src/main/java/modelattribute_demo/controllers/AppController.java
+++ src/main/java/modelattribute_demo/controllers/AppController.java
@@ -2,6 +2,7 @@ package modelattribute_demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import modelattribute_demo.dto.NameInfoDTO;
@@ -9,11 +10,8 @@ import modelattribute_demo.dto.NameInfoDTO;
 @Controller
 public class AppController {
 	@RequestMapping("/home")
-	public String showHomePage(Model model) {
-		// DTO 에서 존재하는 성질 값들을 읽어오라.
-		NameInfoDTO nameInfoDTO = new NameInfoDTO();
-		model.addAttribute("nameInfo", nameInfoDTO);
-
+	public String showHomePage(
+			@ModelAttribute("nameInfo") NameInfoDTO nameInfoDTO) {
 		return "welcome-page";
 	}
 
