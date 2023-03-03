aeb4908a2ecc099f72bc7374ae5c37e61587ca71
osparking <jbpark03@gmail.com>
2023-03-03 11:13:50 +0900

    broken korean fix for post method

--- src/main/java/modelattribute_demo/config/CalculatorAppIntilizer.java
+++ src/main/java/modelattribute_demo/config/CalculatorAppIntilizer.java
@@ -1,5 +1,8 @@
 package modelattribute_demo.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class CalculatorAppIntilizer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -21,5 +24,13 @@ public class CalculatorAppIntilizer extends AbstractAnnotationConfigDispatcherSe
 		String arr[] = { "/modatt.space/*" };
 		return arr;
 	}
+	
+	 @Override
+   protected Filter[] getServletFilters() {
+
+     CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+     characterEncodingFilter.setEncoding("UTF-8");
+     return new Filter[] { characterEncodingFilter};
+   }
 
 }
